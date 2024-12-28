2317ce0cbab2443a53fbd53a2ac6c30d54520c5a
Jonas Møller <jonas@moller.systems>
2024-12-28 01:25:45 +0100

    reformat

--- src/input.rs
+++ src/input.rs
@@ -1,138 +1,137 @@
 use std::time::Instant;
 
 use egui::{pos2, vec2, Key, PointerButton, Pos2, RawInput};
-use winit::{event::MouseButton, keyboard::PhysicalKey};
 use winit::keyboard::{KeyCode, ModifiersState};
+use winit::{event::MouseButton, keyboard::PhysicalKey};
 
 /// Contains and manages everything related to the [`egui`] input
 ///
 /// such as the location of the mouse or the pressed keys
 pub struct Input {
-	dt: Instant,
-	pointer_pos: Pos2,
-	pub(crate) raw: RawInput,
-	pub(crate) scale_factor: f32,
+    dt: Instant,
+    pointer_pos: Pos2,
+    pub(crate) raw: RawInput,
+    pub(crate) scale_factor: f32,
 }
 
 impl Default for Input {
-	/// scale_factor: 1.0
-	fn default() -> Self {
-		Self {
-			dt: Instant::now(),
-			pointer_pos: Default::default(),
-			raw: Default::default(),
-			scale_factor: 1.0,
-		}
+    /// scale_factor: 1.0
+    fn default() -> Self {
+	Self {
+	    dt: Instant::now(),
+	    pointer_pos: Default::default(),
+	    raw: Default::default(),
+	    scale_factor: 1.0,
 	}
+    }
 }
 
 impl Input {
-	pub(crate) fn take(&mut self) -> RawInput {
-		self.raw.predicted_dt = self.dt.elapsed().as_secs_f32();
-		self.dt = Instant::now();
-		self.raw.take()
-	}
+    pub(crate) fn take(&mut self) -> RawInput {
+	self.raw.predicted_dt = self.dt.elapsed().as_secs_f32();
+	self.dt = Instant::now();
+	self.raw.take()
+    }
 
-	/// It updates egui of what is happening in the input (keys pressed, mouse position, etc), but it doesn't updates
-	/// the information of the pressed characters, to update that information you have to
-	/// use the function [text_input_event](Input:: text_input_event)
-	pub fn update(&mut self, ctx: &ggez::Context) {
-		/*======================= Keyboard =======================*/
-		for key in ctx.keyboard.pressed_physical_keys.iter() {
-			if ctx.keyboard.is_physical_key_just_pressed(key) {
-				if let Some(key) = translate_physical_key(*key) {
-					self.raw.events.push(egui::Event::Key {
-						key,
-						pressed: true,
-						repeat: false,
-						modifiers: translate_modifier(ctx.keyboard.active_modifiers),
-					})
-				}
-			}
+    /// It updates egui of what is happening in the input (keys pressed, mouse position, etc), but it doesn't updates
+    /// the information of the pressed characters, to update that information you have to
+    /// use the function [text_input_event](Input:: text_input_event)
+    pub fn update(&mut self, ctx: &ggez::Context) {
+	/*======================= Keyboard =======================*/
+	for key in ctx.keyboard.pressed_physical_keys.iter() {
+	    if ctx.keyboard.is_physical_key_just_pressed(key) {
+		if let Some(key) = translate_physical_key(*key) {
+		    self.raw.events.push(egui::Event::Key {
+			key,
+			pressed: true,
+			repeat: false,
+			modifiers: translate_modifier(ctx.keyboard.active_modifiers),
+		    })
 		}
-
-		/*======================= Mouse =======================*/
-		let ggez::mint::Point2 { x, y } = ctx.mouse.position();
-		self.pointer_pos = pos2(x / self.scale_factor, y / self.scale_factor);
-		self.raw
-			.events
-			.push(egui::Event::PointerMoved(self.pointer_pos));
-
-		for button in [MouseButton::Left, MouseButton::Middle, MouseButton::Right] {
-			if ctx.mouse.button_just_pressed(button) {
-				self.raw.events.push(egui::Event::PointerButton {
-					button: match button {
-						MouseButton::Left => PointerButton::Primary,
-						MouseButton::Right => PointerButton::Secondary,
-						MouseButton::Middle => PointerButton::Middle,
-						_ => unreachable!(),
-					},
-					pos: self.pointer_pos,
-					pressed: true,
-					modifiers: translate_modifier(ctx.keyboard.active_modifiers),
-				});
-			} else if ctx.mouse.button_just_released(button) {
-				self.raw.events.push(egui::Event::PointerButton {
-					button: match button {
-						MouseButton::Left => PointerButton::Primary,
-						MouseButton::Right => PointerButton::Secondary,
-						MouseButton::Middle => PointerButton::Middle,
-						_ => unreachable!(),
-					},
-					pos: self.pointer_pos,
-					pressed: false,
-					modifiers: translate_modifier(ctx.keyboard.active_modifiers),
-				});
-			}
-		}
+	    }
 	}
 
-	/// Set the scale_factor and update the screen_rect
-	pub fn set_scale_factor(&mut self, scale_factor: f32, (w, h): (f32, f32)) {
-		self.scale_factor = scale_factor;
-		self.resize_event(w, h);
-	}
+	/*======================= Mouse =======================*/
+	let ggez::mint::Point2 { x, y } = ctx.mouse.position();
+	self.pointer_pos = pos2(x / self.scale_factor, y / self.scale_factor);
+	self.raw
+	    .events
+	    .push(egui::Event::PointerMoved(self.pointer_pos));
 
-	/// Update screen_rect data with window size
-	pub fn resize_event(&mut self, w: f32, h: f32) {
-		self.raw.screen_rect = Some(egui::Rect::from_min_size(
-			Default::default(),
-			vec2(w, h),
-		));
+	for button in [MouseButton::Left, MouseButton::Middle, MouseButton::Right] {
+	    if ctx.mouse.button_just_pressed(button) {
+		self.raw.events.push(egui::Event::PointerButton {
+		    button: match button {
+			MouseButton::Left => PointerButton::Primary,
+			MouseButton::Right => PointerButton::Secondary,
+			MouseButton::Middle => PointerButton::Middle,
+			_ => unreachable!(),
+		    },
+		    pos: self.pointer_pos,
+		    pressed: true,
+		    modifiers: translate_modifier(ctx.keyboard.active_modifiers),
+		});
+	    } else if ctx.mouse.button_just_released(button) {
+		self.raw.events.push(egui::Event::PointerButton {
+		    button: match button {
+			MouseButton::Left => PointerButton::Primary,
+			MouseButton::Right => PointerButton::Secondary,
+			MouseButton::Middle => PointerButton::Middle,
+			_ => unreachable!(),
+		    },
+		    pos: self.pointer_pos,
+		    pressed: false,
+		    modifiers: translate_modifier(ctx.keyboard.active_modifiers),
+		});
+	    }
 	}
+    }
 
-	/// lets you know the rotation of the mouse wheel
-	pub fn mouse_wheel_event(&mut self, x: f32, y: f32) {
-		self.raw.events.push(egui::Event::Scroll(vec2(x, y)));
-	}
+    /// Set the scale_factor and update the screen_rect
+    pub fn set_scale_factor(&mut self, scale_factor: f32, (w, h): (f32, f32)) {
+	self.scale_factor = scale_factor;
+	self.resize_event(w, h);
+    }
 
-	/// lets know what character is pressed on the keyboard
-	pub fn text_input_event(&mut self, ch: char) {
-		if is_printable(ch) {
-			self.raw.events.push(egui::Event::Text(ch.to_string()));
-		}
+    /// Update screen_rect data with window size
+    pub fn resize_event(&mut self, w: f32, h: f32) {
+	self.raw.screen_rect = Some(egui::Rect::from_min_size(Default::default(), vec2(w, h)));
+    }
+
+    /// lets you know the rotation of the mouse wheel
+    pub fn mouse_wheel_event(&mut self, x: f32, y: f32) {
+	self.raw.events.push(egui::Event::Scroll(vec2(x, y)));
+    }
+
+    /// lets know what character is pressed on the keyboard
+    pub fn text_input_event(&mut self, ch: char) {
+	if is_printable(ch) {
+	    self.raw.events.push(egui::Event::Text(ch.to_string()));
 	}
+    }
 }
 
 #[inline]
-fn translate_physical_key(key: PhysicalKey)-> Option<egui::Key> {
-    let PhysicalKey::Code(key) = key else { return None };
+fn translate_physical_key(key: PhysicalKey) -> Option<egui::Key> {
+    let PhysicalKey::Code(key) = key else {
+	return None;
+    };
     Some(match key {
-        KeyCode::Escape => Key::Escape,
-        KeyCode::Insert => Key::Insert,
-        KeyCode::Home => Key::Home,
-        KeyCode::Delete => Key::Delete,
-        KeyCode::End => Key::End,
-        KeyCode::PageDown => Key::PageDown,
-        KeyCode::PageUp => Key::PageUp,
-        KeyCode::ArrowLeft => Key::ArrowLeft,
-        KeyCode::ArrowUp => Key::ArrowUp,
-        KeyCode::ArrowRight => Key::ArrowRight,
-        KeyCode::ArrowDown => Key::ArrowDown,
-        KeyCode::Backspace => Key::Backspace,
-        KeyCode::Enter => Key::Enter,
-        KeyCode::Tab => Key::Tab,
-        KeyCode::Space => Key::Space,
+	KeyCode::Escape => Key::Escape,
+	KeyCode::Insert => Key::Insert,
+	KeyCode::Home => Key::Home,
+	KeyCode::Delete => Key::Delete,
+	KeyCode::End => Key::End,
+	KeyCode::PageDown => Key::PageDown,
+	KeyCode::PageUp => Key::PageUp,
+	KeyCode::ArrowLeft => Key::ArrowLeft,
+	KeyCode::ArrowUp => Key::ArrowUp,
+	KeyCode::ArrowRight => Key::ArrowRight,
+	KeyCode::ArrowDown => Key::ArrowDown,
+	KeyCode::Backspace => Key::Backspace,
+	KeyCode::Enter => Key::Enter,
+	KeyCode::Tab => Key::Tab,
+	KeyCode::Space => Key::Space,
 
 	KeyCode::KeyA => Key::A,
 	KeyCode::KeyK => Key::K,
@@ -149,13 +148,17 @@ fn translate_physical_key(key: PhysicalKey)-> Option<egui::Key> {
 #[inline]
 fn translate_modifier(keymods: ModifiersState) -> egui::Modifiers {
     egui::Modifiers {
-        alt: keymods.alt_key(),
-        ctrl: keymods.control_key(),
-        shift: keymods.shift_key(),
-        #[cfg(not(target_os = "macos"))] mac_cmd: false,
-        #[cfg(not(target_os = "macos"))] command: keymods.control_key(),
-	#[cfg(target_os = "macos")] mac_cmd: keymods.super_key(),
-	#[cfg(target_os = "macos")] command: keymods.super_key(),
+	alt: keymods.alt_key(),
+	ctrl: keymods.control_key(),
+	shift: keymods.shift_key(),
+	#[cfg(not(target_os = "macos"))]
+		mac_cmd: false,
+	#[cfg(not(target_os = "macos"))]
+		command: keymods.control_key(),
+	#[cfg(target_os = "macos")]
+		mac_cmd: keymods.super_key(),
+	#[cfg(target_os = "macos")]
+		command: keymods.super_key(),
     }
 }
 
@@ -165,5 +168,5 @@ fn is_printable(chr: char) -> bool {
 	|| ('\u{f0000}'..='\u{ffffd}').contains(&chr)
 	|| ('\u{100000}'..='\u{10fffd}').contains(&chr);
 
-	!is_in_private_use_area && !chr.is_ascii_control()
+    !is_in_private_use_area && !chr.is_ascii_control()
 }
--- src/painter.rs
+++ src/painter.rs
@@ -4,173 +4,181 @@ use ggez::graphics::{self, BlendComponent, BlendFactor, BlendMode, BlendOperatio
 
 #[derive(Default, Clone)]
 struct PixBuf {
-	pix: Vec<u8>,
-	w: usize,
-	h: usize
+    pix: Vec<u8>,
+    w: usize,
+    h: usize,
 }
 
 #[derive(Default, Clone)]
 pub struct Painter {
-	pub(crate) shapes: Vec<egui::ClippedPrimitive>,
-	pub(crate) textures_delta: LinkedList<egui::TexturesDelta>,
-	paint_jobs: Vec<(egui::TextureId, graphics::Mesh, graphics::Rect)>,
-	textures: HashMap<egui::TextureId, graphics::Image>,
-	images: HashMap<egui::TextureId, PixBuf>,
+    pub(crate) shapes: Vec<egui::ClippedPrimitive>,
+    pub(crate) textures_delta: LinkedList<egui::TexturesDelta>,
+    paint_jobs: Vec<(egui::TextureId, graphics::Mesh, graphics::Rect)>,
+    textures: HashMap<egui::TextureId, graphics::Image>,
+    images: HashMap<egui::TextureId, PixBuf>,
 }
 
 impl Painter {
-	pub fn draw(&mut self, canvas: &mut graphics::Canvas, scale_factor: f32) {
-		let prev_blend = canvas.blend_mode();
-		canvas.set_blend_mode(BlendMode {
-			color: BlendComponent {
-				src_factor: BlendFactor::One,
-				dst_factor: BlendFactor::OneMinusSrcAlpha,
-				operation: BlendOperation::Add,
-			},
-			alpha: BlendComponent {
-				src_factor: BlendFactor::OneMinusDstAlpha,
-				dst_factor: BlendFactor::One,
-				operation: BlendOperation::Add,
-			},
-		});
-		for (id, mesh, clip) in self.paint_jobs.iter() {
-			canvas.set_scissor_rect(*clip).unwrap();
-			canvas.draw_textured_mesh(
-				mesh.clone(),
-				self.textures[id].clone(),
-				graphics::DrawParam::default().scale([scale_factor, scale_factor]),
-			);
-		}
-		canvas.set_default_scissor_rect();
-		canvas.set_blend_mode(prev_blend);
+    pub fn draw(&mut self, canvas: &mut graphics::Canvas, scale_factor: f32) {
+	let prev_blend = canvas.blend_mode();
+	canvas.set_blend_mode(BlendMode {
+	    color: BlendComponent {
+		src_factor: BlendFactor::One,
+		dst_factor: BlendFactor::OneMinusSrcAlpha,
+		operation: BlendOperation::Add,
+	    },
+	    alpha: BlendComponent {
+		src_factor: BlendFactor::OneMinusDstAlpha,
+		dst_factor: BlendFactor::One,
+		operation: BlendOperation::Add,
+	    },
+	});
+	for (id, mesh, clip) in self.paint_jobs.iter() {
+	    canvas.set_scissor_rect(*clip).unwrap();
+	    canvas.draw_textured_mesh(
+		mesh.clone(),
+		self.textures[id].clone(),
+		graphics::DrawParam::default().scale([scale_factor, scale_factor]),
+	    );
+	}
+	canvas.set_default_scissor_rect();
+	canvas.set_blend_mode(prev_blend);
+    }
+
+    pub fn clear(&mut self) {
+	self.paint_jobs.clear();
+    }
+
+    pub fn update(&mut self, ctx: &mut ggez::Context, scale_factor: f32) {
+	// Create and free textures
+	while let Some(textures_delta) = self.textures_delta.pop_front() {
+	    self.update_textures(ctx, textures_delta);
 	}
 
-	pub fn clear(&mut self) {
-		self.paint_jobs.clear();
-	}
-
-	pub fn update(&mut self, ctx: &mut ggez::Context, scale_factor: f32) {
-		// Create and free textures
-		while let Some(textures_delta) = self.textures_delta.pop_front() {
-			self.update_textures(ctx, textures_delta);
-		}
-
-		// generating meshes
-		for egui::ClippedPrimitive {
-			primitive,
-			clip_rect,
-		} in self.shapes.iter()
+	// generating meshes
+	for egui::ClippedPrimitive {
+	    primitive,
+	    clip_rect,
+	} in self.shapes.iter()
 		{
-			match primitive {
-				egui::epaint::Primitive::Mesh(mesh) => {
-					if mesh.vertices.len() < 3 {
-						continue;
-					}
+		    match primitive {
+			egui::epaint::Primitive::Mesh(mesh) => {
+			    if mesh.vertices.len() < 3 {
+				continue;
+			    }
 
-					let vertices = mesh
-						.vertices
-						.iter()
-						.map(|v| graphics::Vertex {
-							position: [v.pos.x, v.pos.y],
-							uv: [v.uv.x, v.uv.y],
-							color: egui::Rgba::from(v.color).to_array(),
-						})
-						.collect::<Vec<_>>();
+			    let vertices = mesh
+				.vertices
+				.iter()
+				.map(|v| graphics::Vertex {
+				    position: [v.pos.x, v.pos.y],
+				    uv: [v.uv.x, v.uv.y],
+				    color: egui::Rgba::from(v.color).to_array(),
+				})
+				.collect::<Vec<_>>();
 
-					self.paint_jobs.push((
+			    self.paint_jobs.push((
 						mesh.texture_id,
 						graphics::Mesh::from_data(
-							ctx,
-							graphics::MeshData {
-								vertices: vertices.as_slice(),
-								indices: mesh.indices.as_slice(),
-							},
+						    ctx,
+						    graphics::MeshData {
+							vertices: vertices.as_slice(),
+							indices: mesh.indices.as_slice(),
+						    },
 						),
 						graphics::Rect::new(
-							clip_rect.min.x * scale_factor,
-							clip_rect.min.y * scale_factor,
-							(clip_rect.max.x - clip_rect.min.x) * scale_factor,
-							(clip_rect.max.y - clip_rect.min.y) * scale_factor,
+						    clip_rect.min.x * scale_factor,
+						    clip_rect.min.y * scale_factor,
+						    (clip_rect.max.x - clip_rect.min.x) * scale_factor,
+						    (clip_rect.max.y - clip_rect.min.y) * scale_factor,
 						),
-					));
-				}
-				egui::epaint::Primitive::Callback(_) => {
-					panic!("Custom rendering callbacks are not implemented yet");
-				}
+			    ));
 			}
+			egui::epaint::Primitive::Callback(_) => {
+			    panic!("Custom rendering callbacks are not implemented yet");
+			}
+		    }
 		}
+    }
+
+    pub fn update_textures(
+	&mut self,
+	ctx: &mut ggez::Context,
+	textures_delta: egui::TexturesDelta,
+    ) {
+	// set textures
+	for (id, delta) in &textures_delta.set {
+	    let pixbuf = PixBuf::from_image_data(&delta.image);
+	    if let Some(pos) = delta.pos {
+		eprintln!("Error: Non-zero offset texture updates are not implemented yet");
+		let Some(mut img) = self.images.remove(id) else {
+		    eprintln!("Got update request for unknown image");
+		    continue;
+		};
+		img.blit(&pixbuf, (pos[0], pos[1]));
+	    } else {
+		self.textures.insert(*id, pixbuf.to_texture(ctx));
+		self.images.insert(*id, pixbuf);
+	    }
 	}
 
-	pub fn update_textures(
-		&mut self,
-		ctx: &mut ggez::Context,
-		textures_delta: egui::TexturesDelta,
-	) {
-		// set textures
-		for (id, delta) in &textures_delta.set {
-			let pixbuf = PixBuf::from_image_data(&delta.image);
-			if let Some(pos) = delta.pos {
-				eprintln!("Error: Non-zero offset texture updates are not implemented yet");
-				let Some(mut img) = self.images.remove(id) else {
-					eprintln!("Got update request for unknown image");
-					continue;
-				};
-				img.blit(&pixbuf, (pos[0], pos[1]));
-			} else {
-				self.textures.insert(*id, pixbuf.to_texture(ctx));
-				self.images.insert(*id, pixbuf);
-			}
-		}
-
-		// free textures
-		for id in &textures_delta.free {
-			self.textures.remove(id);
-			self.images.remove(id);
-		}
+	// free textures
+	for id in &textures_delta.free {
+	    self.textures.remove(id);
+	    self.images.remove(id);
 	}
+    }
 }
 
 impl PixBuf {
-	fn from_color(color: &egui::ColorImage) -> Self {
-		let mut pix: Vec<u8> = Vec::with_capacity(color.pixels.len() * 4);
-		for pixel in &color.pixels {
-			pix.extend(pixel.to_array());
-		}
-		Self { pix, w: color.width(), h: color.height() }
+    fn from_color(color: &egui::ColorImage) -> Self {
+	let mut pix: Vec<u8> = Vec::with_capacity(color.pixels.len() * 4);
+	for pixel in &color.pixels {
+	    pix.extend(pixel.to_array());
 	}
+	Self {
+	    pix,
+	    w: color.width(),
+	    h: color.height(),
+	}
+    }
 
-	fn from_font(font: &egui::FontImage) -> Self {
-		let mut pix: Vec<u8> = Vec::with_capacity(font.pixels.len() * 4);
-		for pixel in font.srgba_pixels(None) {
-			pix.extend(pixel.to_array());
-		}
-		Self { pix, w: font.width(), h: font.height() }
+    fn from_font(font: &egui::FontImage) -> Self {
+	let mut pix: Vec<u8> = Vec::with_capacity(font.pixels.len() * 4);
+	for pixel in font.srgba_pixels(None) {
+	    pix.extend(pixel.to_array());
 	}
+	Self {
+	    pix,
+	    w: font.width(),
+	    h: font.height(),
+	}
+    }
 
-	fn from_image_data(img: &egui::ImageData) -> Self {
-		match &img {
-			egui::ImageData::Color(image) => Self::from_color(image),
-			egui::ImageData::Font(image) => Self::from_font(image),
-		}
+    fn from_image_data(img: &egui::ImageData) -> Self {
+	match &img {
+	    egui::ImageData::Color(image) => Self::from_color(image),
+	    egui::ImageData::Font(image) => Self::from_font(image),
 	}
+    }
 
-	fn to_texture(&self, ctx: &mut ggez::Context) -> graphics::Image {
-		graphics::Image::from_pixels(
-			ctx,
-			self.pix.as_slice(),
-			graphics::ImageFormat::Rgba8UnormSrgb,
-			self.w as u32,
-			self.h as u32,
-		)
-	}
+    fn to_texture(&self, ctx: &mut ggez::Context) -> graphics::Image {
+	graphics::Image::from_pixels(
+	    ctx,
+	    self.pix.as_slice(),
+	    graphics::ImageFormat::Rgba8UnormSrgb,
+	    self.w as u32,
+	    self.h as u32,
+	)
+    }
 
-	fn blit(&mut self, pix: &PixBuf, pos: (usize, usize)) {
-		for row in pos.1..pos.1+pix.h {
-			let dst = row*self.w + pos.0;
-			let src = row*pix.h;
-			for (i, j) in (dst..dst+pix.w).zip(src..src+pix.w) {
-				println!("{i} <- {j}");
-			}
-		}
+    fn blit(&mut self, pix: &PixBuf, pos: (usize, usize)) {
+	for row in pos.1..pos.1 + pix.h {
+	    let dst = row * self.w + pos.0;
+	    let src = row * pix.h;
+	    for (i, j) in (dst..dst + pix.w).zip(src..src + pix.w) {
+		println!("{i} <- {j}");
+	    }
 	}
+    }
 }
